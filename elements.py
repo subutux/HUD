478becbc7b05ada49576adf27f32e9156bb3b7ab
Stijn Van Campenhout <subutux@gmail.com>
2016-10-09 00:30:55 +0200

    Updated switch Element

--- elements.py
+++ elements.py
@@ -73,15 +73,15 @@ class Light(gui.Button):
 #		if self.haevent.state == hasconst.STATE_ON: img = self.style.down
 #		s.blit(img,(0,0))		
 
-class LightSwitch(gui.Button):
+class LightSwitch(gui.Switch):
 	def __init__(self,api,haevent,**kwargs):
 		self.api = api
 		self.haevent = None
-		super().__init__("--",**kwargs)
 
-		self.set_hass_event(haevent)
+		super().__init__(value=False,**kwargs)
+
 		self.connect(gui.CLICK,self.callback)
-
+		self.set_hass_event(haevent)
 	def callback(self):
 		print('callback')
 		if self.haevent.state == hasconst.STATE_OFF:
@@ -94,50 +94,21 @@ class LightSwitch(gui.Button):
 				})
 		print (status);
 		# TODO: Fix time
-		self.set_hass_event(remote.get_state(self.api,self.haevent.entity_id))
-
+		self.update_hass_event()
+	def click(self):
+		pass
 	def set_hass_event(self,haevent):
 		self.haevent = haevent;
 		print(haevent)
 		if self.haevent.state == hasconst.STATE_OFF:
 			print("off")
-			self.state = 0
-			self.pcls = ""
+			self._value = False
 		elif self.haevent.state == hasconst.STATE_ON:
 			print("on")
-			self.state = 1
-			self.pcls = "down"
+			self._value = True
 		self.repaint()
-	def update_hass_event():
+	def update_hass_event(self):
 		self.set_hass_event(remote.get_state(self.api,self.haevent.entity_id))		
-	def event(self,e):
-
-		if e.type == ENTER: self.repaint()
-		elif e.type == EXIT: self.repaint()
-		elif e.type == FOCUS: self.repaint()
-		elif e.type == BLUR: self.repaint()
-		elif e.type == KEYDOWN:
-		    if e.key == K_SPACE or e.key == K_RETURN:
-		        self.state = 1
-		        self.repaint()
-		elif e.type == MOUSEBUTTONDOWN:
-		    self.state = 1
-		    self.repaint()
-		elif e.type == KEYUP:
-		    if self.state == 1:
-		        sub = pygame.event.Event(CLICK,{'pos':(0,0),'button':1})
-		        #self.send(sub.type,sub)
-		        self._event(sub)
-		        #self.event(sub)
-		        #self.click()
-
-		    #self.state = 0
-		    self.repaint()
-		elif e.type == MOUSEBUTTONUP:
-		    #self.state = 0
-		    self.repaint()
-		elif e.type == CLICK:
-		    self.click()
 		
 #		if self.haevent.state == hasconst.STATE_ON: img = self.style.down
 #		s.blit(img,(0,0))		
